29c5075146a8898846a9519e7cfda1a701901b25
zhang tian <975571814@qq.com>
2018-03-31 10:31:38 +0800

    PracticeB is finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,22 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> results = new ArrayList<>();
+        PracticeA practiceA = new PracticeA();
+
+        for (String str : collection1) {
+            for (List<String> list : collection2) {
+                if (practiceA.includeElement(list, str)) {
+                    results.add(str);
+                    break;
+                }
+            }
+        }
+
+        return results;
     }
 }
