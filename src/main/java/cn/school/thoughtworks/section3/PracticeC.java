c2ca056ba4d7cfad827c5581b94cf1294057d858
zhang tian <975571814@qq.com>
2018-03-31 12:30:00 +0800

    section3 PracticeB is finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,39 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import cn.school.thoughtworks.section2.PracticeA;
+
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+        PracticeB practiceB = new PracticeB();
 
-        return null;
+        Map<String, Integer> collection = countSameElements(collectionA);
+
+        return practiceB.createUpdatedCollection(collection, object);
+    }
+
+    Map<String,Integer> countSameElements(List<String> collection1) {
+        Map<String, Integer> results = new HashMap<>();
+
+        for (String str : collection1) {
+            results.put(str, countElementsInMap(results, str));
+        }
+
+        return results;
+    }
+
+    int countElementsInMap(Map<String, Integer> collection, String element) {
+        int count = 0;
+
+        if (collection.get(element) == null) {
+            count = 1;
+        } else {
+            count = collection.get(element) + 1;
+        }
+
+        return count;
     }
 }
