b4f9b45532a97b9bc86f4d68b6fccfac3afb1f12
zhang tian <975571814@qq.com>
2018-03-31 10:22:29 +0800

    PracticeA collectSameElements finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,29 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> results = new ArrayList<>();
+
+        for (String str : collection1) {
+            if (includeElement(collection2, str)) {
+                results.add(str);
+            }
+        }
+
+        return results;
+    }
+
+    Boolean includeElement(List<String> collection, String element) {
+        for (String str : collection) {
+            if (element.equals(str)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
