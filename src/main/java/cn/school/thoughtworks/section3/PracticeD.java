f2c2980cb9b565d9deb16b6e392abee5a8fdfd33
zhang tian <975571814@qq.com>
2018-03-31 12:30:44 +0800

    section3 PracticeD is finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,35 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeA;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+    private PracticeB practiceB = new PracticeB();
+    private PracticeC practiceC = new PracticeC();
 
-        return null;
+    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+        Map<String, Integer> collection = formatCollection(collectionA);
+
+        return practiceB.createUpdatedCollection(collection, object);
+    }
+
+    Map<String, Integer> formatCollection(List<String> collection) {
+        Map<String, Integer> results = new HashMap<>();
+        for (String str : collection) {
+            int count = 0;
+            if (str.length() > 1) {
+                String[] array = str.split("-");
+                str = array[0];
+                count = Integer.parseInt(array[1]);
+            }
+            results.put(str, practiceC.countElementsInMap(results, str) + count);
+        }
+
+        return results;
     }
 }
