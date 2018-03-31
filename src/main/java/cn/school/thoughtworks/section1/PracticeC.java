ee56b97a064d2315c1781a8ac51fdc61ab5b2b8d
zhang tian <975571814@qq.com>
2018-03-31 10:31:56 +0800

    PracticeC is finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> results = new ArrayList<>();
+        PracticeA practiceA = new PracticeA();
+
+        for (String str : collection1) {
+            if (practiceA.includeElement(collection2.get("value"), str)) {
+                results.add(str);
+            }
+        }
+
+        return results;
     }
 }
