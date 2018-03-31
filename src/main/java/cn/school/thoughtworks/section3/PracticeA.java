a4a75cd6473b667dd7b5edddc5154025847f4ffa
zhang tian <975571814@qq.com>
2018-03-31 12:29:40 +0800

    section3 PracticeA is finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -5,8 +5,13 @@ import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
 
-        return null;
+        for (String str : object.get("value")) {
+            if (collectionA.get(str) != null) {
+                collectionA.put(str, collectionA.get(str) - 1);
+            }
+        }
+
+        return collectionA;
     }
 }
