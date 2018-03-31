869d3e7d99087682681ad7bae5bc53d4d5b9d09d
zhang tian <975571814@qq.com>
2018-03-31 10:43:27 +0800

    section2 PracticeA finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,29 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> results = new HashMap<>();
 
-        return null;
+        for (String str : collection1) {
+            results.put(str, countElementsInMap(results, str));
+        }
+
+        return results;
+    }
+
+    int countElementsInMap(Map<String, Integer> collection, String element) {
+        int count = 0;
+
+        if (collection.get(element) == null) {
+            count = 1;
+        } else {
+            count = collection.get(element) + 1;
+        }
+
+        return count;
     }
 }
