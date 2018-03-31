7810f879d211d1e20d4efe4065152824b9689e52
zhang tian <975571814@qq.com>
2018-03-31 11:56:52 +0800

    section2 PracticeB is finish
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,61 @@
 package cn.school.thoughtworks.section2;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> results = new HashMap<>();
+        PracticeA practiceA = new PracticeA();
 
-        return null;
+        collection1 = formatCollection(collection1);
+
+        for (String str : collection1) {
+            results.put(str, practiceA.countElementsInMap(results, str));
+        }
+
+        return results;
+    }
+
+    List<String> formatCollection(List<String> collection) {
+        List<String> results = new ArrayList<>();
+
+        for (String string : collection) {
+            if (string.length() > 1) {
+                String splitStr = String.valueOf(string.charAt(1));
+                if (splitStr.equals("[")) {
+                    splitStr = "\\[";
+                }
+                String[] array = string.split(splitStr);
+                int number = formatNumber(array[1]);
+                int i = 0;
+                while (i < number) {
+                    results.add(array[0]);
+                    i ++;
+                }
+            } else {
+                results.add(string);
+            }
+        }
+
+        return results;
+    }
+
+    int formatNumber(String string) {
+        String number = "";
+        for (String s : string.split("")) {
+            if (!StringUtils.isNumeric(s)) {
+                break;
+            }
+
+            number += s;
+        }
+
+        return Integer.parseInt(number);
     }
 }
